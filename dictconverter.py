67966448458c9a084aac4cec340ffbcc6a442eeb
deifius <sunji.roaoul@gmail.com>
2017-12-05 21:18:46 -0700

    converted all ListClasses to DictClasses for preparation of ClassClasses

---
+++ dictconverter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json 
+import sys
+import pdb
+
+charclass = list(zip(*json.loads(open('json/'+sys.argv[1]+'.json','r').readline())))
+
+characterdict = {}
+
+
+
+if len(charclass[-1][-1]) == 1: 
+	characterdict['SpellsPerLevel'] = []
+	while len(charclass[-1][-1]) == 1: 
+		characterdict['SpellsPerLevel'].insert(0,charclass.pop(-1))
+	characterdict["SpellsPerLevel"] = list(zip(*characterdict["SpellsPerLevel"]))
+
+characterdict['SpecialAbilities'] = charclass.pop(-1)
+characterdict['Will'] = charclass.pop(-1)
+characterdict['Reflex'] = charclass.pop(-1)
+characterdict['Fortitude'] = charclass.pop(-1)
+characterdict['BaseAttackBonus'] = charclass.pop(-1)
+
+
+dictatedchar = open("jsondicts/"+sys.argv[1]+".json",'w')
+dictatedchar.write(json.dumps(characterdict))
+dictatedchar.close()
